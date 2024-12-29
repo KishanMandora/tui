b98741014431ef2137798d71d46776133d638866
Kishan Mandora <kishanmandora7@gmail.com>
2024-12-29 21:40:40 +0530

    docs(changeset): change btn styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,7 +5,7 @@ type ButtonProps = {
 
 function Button({ text, color }: ButtonProps) {
   return (
-    <button style={{ color, padding: ".25rem .5rem", border: "none" }}>{text}</button>
+    <button style={{ color, padding: ".5rem 1rem", border: "none" }}>{text}</button>
   )
 }
 
